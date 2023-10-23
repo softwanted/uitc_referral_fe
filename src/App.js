2d4011f617a613e545e8c1df5c45d76568eec5dc
OlehS <olehsjs@gmail.com>
2023-10-23 02:21:22 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="h1">
+      <header>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello UITC!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
